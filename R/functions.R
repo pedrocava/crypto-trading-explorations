cd7a5165ced6377f61feba5dcea41b5ecbb59da2
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2021-09-14 20:03:48 -0300

    um pouco de visualização, processamento, criação de features

--- R/functions.R
+++ R/functions.R
@@ -7,6 +7,71 @@ bitcoin_data <- function() {
   
 }
 
+feature_enrichment <- function(data) {
+  
+  data %>%
+    dplyr::arrange(timestamp) %>%
+    dplyr::mutate(
+      return = (price - dplyr::lag(price)) / price,
+      return_mvavg = slider::slide_dbl(
+        .x = return,
+        .f = mean,
+        .before = 7),
+      price_vol = slider::slide_dbl(
+        .x = price,
+        .f = sd,
+        .before = 7),
+      price_mvavg = slider::slide_dbl(
+        .x = price,
+        .f = mean,
+        .before = 7))
+  
+}
+
+cummulative_return <- function(features) {
+  
+  features %>%
+    dplyr::select(timestamp, return) %>%
+    tidyr::drop_na() %>%
+    dplyr::mutate(cumreturn = purrr::accumulate(1 + return, `*`))
+  
+}
+
+
+
+viz_return_dist <- function(features) {
+  
+  features %>%
+    ggplot2::ggplot(ggplot2::aes(x = return)) + 
+    ggplot2::geom_histogram(binwidth = .01, fill = 'light blue') +
+    ggplot2::theme_minimal() + 
+    ggplot2::labs(
+      x = "Returns",
+      y = "Count",
+      title = "Histograma dos Retornos de BTC nos últimos 180 dias")
+    
+}
+
+viz_historic_series <- function(features) {
+  
+  features %>%
+    dplyr::select(timestamp, price_mvavg, price) %>%
+    tidyr::pivot_longer(
+      cols = c(price_mvavg, price),
+      names_to = "series",
+      values_to = "value") %>%
+    ggplot2::ggplot(ggplot2::aes(x = timestamp, y = value, group = series)) +
+    ggplot2::geom_line(size = 1.2, color = 'red') + 
+    ggplot2::theme_minimal() + 
+    ggplot2::labs(
+      title = "Preços e média móvel semanal",
+      x = "Tempo",
+      y = "Valores",
+      legend = "Séries")
+  
+}
+
+
 initial_split <- function(data) {
   
   rsample::initial_time_split(data, prop = .9)
@@ -17,9 +82,14 @@ model_frame <- function(splits) {
   
   model_fit_prophet <- modeltime::prophet_reg() %>%
     parsnip::set_engine(engine = "prophet") %>%
-    recipes::step_naomit(timestamp) %>%
-    recipes::step_date(timestamp, feature = 'month', ordinal = FALSE) %>%
-    parsnip::fit(price ~ timestamp, data = rsample::training(splits))
+    recipes::step_naomit(c(timestamp, return, price)) %>%
+    recipes::step_date(
+      timestamp,
+      feature = 'month',
+      ordinal = FALSE) %>%
+    parsnip::fit(
+      return ~ timestamp + log(price) + price_vol,
+      data = rsample::training(splits))
   
   modeltime::modeltime_table(model_fit_prophet)
   
--- _targets.R
+++ _targets.R
@@ -1,13 +1,18 @@
 library(targets)
 source("R/functions.R")
+options(warning = FALSE)
 
 targets::tar_option_set(
   packages = c("geckor", "tidyverse", "tsibble", "slider", "modeltime"))
 
 list(
   targets::tar_target(data, bitcoin_data()),
-  targets::tar_target(splits, initial_split(data)),
+  targets::tar_target(features, feature_enrichment(data)),
+  targets::tar_target(return_histogram, viz_return_dist(features)),
+  targets::tar_target(historic_prices, viz_historic_series(features)),
+  targets::tar_target(cummulative_return, cummulative_return(features)),
+  targets::tar_target(splits, initial_split(features)),
   targets::tar_target(models, model_frame(splits)),
   targets::tar_target(calibration, model_calibration(splits, models)),
-  targets::tar_target(forecast_plot, visualize_forecast(calibration, splits, data))
+  targets::tar_target(forecast_plot, visualize_forecast(calibration, splits, features))
 )
